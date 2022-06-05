4a1d23eb874d7eda437631e48187adfd40a73306
Michael Dwyer <Mike.a.dwyer32@gmail.com>
2022-06-05 14:35:05 -0400

    Just added the delete button and about to create the patch/update

--- anime-fantasy-client/src/components/ALCreateForm.js
+++ anime-fantasy-client/src/components/ALCreateForm.js
@@ -45,7 +45,7 @@ const ALCreateForm = () => {
         <form className='shadow-md bg-white round box-border h-64 w-96 p-4 px-8 pt-6 pb-8 mb-4 mt-20' onSubmit={handleSubmit} >
         <h2>Create League</h2>
             <label className='black text-gray-700 tet-sm font-bold md-2' >League Name</label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name='name' onChange={handleChange} value={league.name} type='text' placeholder='League Name'/>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name='name' onChange={handleChange} value={league.name} require type='text' placeholder='League Name'/>
             <br/>
             <div className='flex items-center justify-between mt-5'>
             <input className='bg-blue-500 hover:bg-blue-800 hover:text-white font-bold py-2 px-4 border rounded focus:outline-none focus:shadow-outline' type='submit' value='Start'/>
--- anime-fantasy-client/src/components/LeagueCard.js
+++ anime-fantasy-client/src/components/LeagueCard.js
@@ -1,31 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { UserContext } from '../context/user';
 import { useNavigate } from 'react-router';
 
 
-const LeagueCard = ({leagues}) => {
+const LeagueCard = ({ leagues }) => {
 
-    console.log('Leagues names', leagues);
+    // console.log('Leagues names', leagues);
     const { user } = useContext(UserContext)
-
-
+    const [leagueCard, setLeagueCard] = useState(leagues)
     const history = useNavigate()
 
-    console.log('Leagues Invitations Data INSIDE LeagueCard:', leagues[0]?.league_characters);
+    function handleDelete(leagueId) {
+        fetch(`/league/${leagueId}`, {method: 'DELETE',})
+        .then(r => r.json())
+        .then(deletedLeague => setLeagueCard(leagueCard.filter(lc => lc.id !== deletedLeague.id)))
+    }
+
+    // console.log('Leagues Invitations Data INSIDE LeagueCard:', leagues[0]?.league_characters);
 
     const leagueMapped = () => {
         return leagues.map((league) => (
             <>
-                <div className='grid grid-cols-3 shadow-md bg-white rounded-lg box-border hover:bg-blue-400 hover:text-blue-50 hover:-translate-y-3 duration-500 h-300 w-96 p-4 px-8 pt-6 pb-8 my-12' onClick={() => history(`/anime-league/${league.id}`)}>
-                    <div>                        
-                        <h1 key={league.id} className='text-blue-600 text-xs font-bold px-2 py-2'>{league.name}</h1>
+                <div className=' shadow-md bg-white rounded-lg box-border hover:bg-blue-400 hover:-translate-y-3 duration-500 h-300 w-96 p-4 px-8 pt-6 pb-8 my-12' onClick={() => history(`/anime-league/${league.id}`)}>
+                    <div className='hover:text-blue-50'>                        
+                        <h1 key={league.id} className='hover:text-blue-50 text-blue-600 text-xs font-bold px-2 py-2'>{league.name}</h1>
                         <h3 className='text-blue-600 text-xs font-bold px-2 py-2'>
-                            Members: {league.invitations.map((i) => {return ` ${i.member.username}  `;})}                    
+                            Members: {league.invitations.map((i) => {return `${i.member.username}\r\n`;})}                    
                         </h3>
-                        { user.id === league.creator_id ? 'DELETE' : ''}
-                        {/* <>
-                            Characters: {league.league_characters.map((lc) => { return <img src={require(`${lc.character.img}`)}/>})}
-                        </> */}
+                        { user.id === league.creator_id ? <input className='bg-blue-600 rounded-lg box-border hover:bg-blue-400 hover:text-blue-600 text-blue-50 text-xs font-bold px-2 py-2' onClick={() => handleDelete(league.id)} type='submit' value='Delete'/> : ''}
+                        <br/>
+                        { user.id === league.creator_id ? 'EDIT' : ''}
                     </div>
                 </div>                
             </>
